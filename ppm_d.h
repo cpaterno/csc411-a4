a6b909d12cd83ffe5c8e301c08d2fec97944a58e
cpaterno <cpaterno@my.uri.edu>
2019-10-26 21:01:32 -0400

    Header created

---
+++ ppm_d.h
@@ -0,0 +1,27 @@
+#ifndef __PPM_D_
+#define __PPM_D__
+
+#include "pnm.h"
+
+// Module representing the decompressed ppm
+// This module builds off of pnm.h and exports 4 functions and one data structure
+
+// double version of Pnm_rgb used for float representation of the image's pixels
+// exported as pointer to follow pnm interface's format
+typdef struct Pnm_rgb_f {
+    double r, g, b;
+} *Pnm_rgb_f; 
+
+// count the number of 2 by 2 blocks in an even dimensioned image
+extern unsigned num_blocks(Pnm_ppm ppm);
+
+// return a new ppm which has even dimensions
+Pnm_ppm trim(const Pnm_ppm ppm);
+
+// return a new ppm which has pixels with floating point representation
+Pnm_ppm floatrep(const Pnm_ppm ppm);
+
+// return a new ppm which has pixels with int representation
+Pnm_ppm intrep(const Pnm_ppm ppm);
+
+#endif
